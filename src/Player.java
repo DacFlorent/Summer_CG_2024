79d7c507470604bbbd52cd248d9e9f483b1c7390
DacFlorent <Dac.florent@gmail.com>
2024-06-12 18:40:51 +0200

    rework depuis fichier jeu 1

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Player {
 
@@ -8,268 +7,69 @@ class Player {
         int playerIdx = in.nextInt();
         int nbGames = in.nextInt();
         if (in.hasNextLine()) {
-            in.nextLine();
+            in.nextLine(); // Consume newline character
         }
 
         // game loop
         while (true) {
+            // Read score information for each player
+            String[] scoreInfo = new String[3];
             for (int i = 0; i < 3; i++) {
-                String scoreInfo = in.nextLine();
+                scoreInfo[i] = in.nextLine();
             }
 
-            // listes de stockage GPUs et Reg positions
-            List<String> gpus = new ArrayList<>();
-            List<Integer[]> positionRegisters = new ArrayList<>();
-            List<Integer[]> stunRegisters = new ArrayList<>();
-
+            // Read game data for each mini-game
+            String[] gpu = new String[nbGames];
+            int[][] registers = new int[nbGames][7]; // 7 registers (reg0 to reg6)
             for (int i = 0; i < nbGames; i++) {
-                String gpu = in.next();
-                int reg0 = in.nextInt(); // position player 1
-                int reg1 = in.nextInt(); // position player 2
-                int reg2 = in.nextInt(); // position player 3
-                int reg3 = in.nextInt(); // stun player 1
-                int reg4 = in.nextInt(); // stun player 2
-                int reg5 = in.nextInt(); // stun player 3
-                int reg6 = in.nextInt(); // useless
-
-                gpus.add(gpu);
-
-                //choix du reg en fonction du PlayerIdx
-                Integer[] positionReg;
-                Integer[] stunReg;
-
-                switch (playerIdx) {
-                    case 0:
-                        positionReg = new Integer[]{reg0};
-                        stunReg = new Integer[]{reg3};
-                        break;
-                    case 1:
-                        positionReg = new Integer[]{reg1};
-                        stunReg = new Integer[]{reg4};
-                        break;
-                    case 2:
-                        positionReg = new Integer[]{reg2};
-                        stunReg = new Integer[]{reg5};
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Unexpected value: " + playerIdx);
+                gpu[i] = in.next();
+                for (int j = 0; j < 7; j++) {
+                    registers[i][j] = in.nextInt();
                 }
-                positionRegisters.add(positionReg);
-                stunRegisters.add(stunReg);
-
-
-                // debug System.err.println("message")
-                System.err.println("GPU : " + gpu);
-                System.err.println("Position Register : " + Arrays.toString(positionReg));
-                System.err.println("Stun Register : " + Arrays.toString(stunReg));
-            }
-            in.nextLine();
-
-            // Boucle de calcul par itération
-            for (int i = 0; i < nbGames; i++) {
-                String gpu = gpus.get(i);
-                Integer[] positionReg = positionRegisters.get(i);
-                List<Action> actionsPossibles = calculatePossibleActions(gpu, positionReg);
-
-                System.err.println("Game " + i + "GPU = " + gpu + ", Actions possibles = " + actionsPossibles);
+                if (in.hasNextLine()) {
+                    in.nextLine(); // Consume newline character
+                }
             }
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
+            // Choose the action for the current game state
+            String direction = chooseDirection(playerIdx, gpu[0], registers[playerIdx][0], registers[playerIdx][3]);
 
-            System.out.println("LEFT");
+            // Output the chosen direction
+            System.out.println(direction);
         }
     }
 
-    // Methode de calcul par itération
-    public static List<Action> calculatePossibleActions(String gpu, Integer[] reg) {
-        List<Action> actions = new ArrayList<>();
-
-
-        // distance du prochain #
-        // ....#.#..
-        int distanceProchainObstacle = gpu.indexOf("#", reg[0]);
-
-        if (distanceProchainObstacle > 0) {
-            actions.add(Action.UP);
-        } else {
-            int distanceAAvance = 2;
-
-            if (distanceProchainObstacle > 1) {
-                actions.add(Action.DOWN);
-            } else {
-                actions.add(Action.LEFT);
-            }
+    private static String chooseDirection(int playerIdx, String gpu, int myPosition, int myStunCount) {
+        // If stunned, do nothing
+        if (myStunCount > 0) {
+            return "LEFT";
         }
 
-        return actions;
+        // Check the track length
+        int trackLength = gpu.length();
 
-    }
-
-        private static String chooseDirection (String[]gpus,int[][] registers){
-            // Determine the games to consider (the 3 games with the least obstacles)
-            List<Integer> gamesToConsider = getGamesToConsider(gpus);
-
-            // Check each game's state to decide the action
-            for (int i : gamesToConsider) {
-                String gpu = gpus[i];
-                int position = registers[i][0];
-                int stunCount = registers[i][3];
-
-                // If the game is stunned, skip it
-                if (stunCount > 0) {
-                    continue;
-                }
-
-                // Check if there's an obstacle in the next cell
-                if (position < gpu.length() - 1 && gpu.charAt(position + 1) == '#') {
-                    // Try to jump over the obstacle
-                    registers[i][0]++; // Move position forward
-
-                    // Check if we can do a double jump (obstacle two cells ahead)
-                    if (position < gpu.length() - 2 && gpu.charAt(position + 2) == '#') {
-                        registers[i][0]++; // Move position forward again
-                        return "UP";
-                    } else {
-                        return "UP";
-                    }
-                }
-
-                // Check if there's an obstacle two cells ahead for double jump
-                if (position < gpu.length() - 2 && gpu.charAt(position + 2) == '#') {
-                    registers[i][0] += 2; // Move position forward
+        // Check the next cells for obstacles
+        if (myPosition < trackLength - 1) {
+            char nextCell = gpu.charAt(myPosition + 1);
+            if (nextCell == '#') {
+                // If next cell is a hurdle, jump over it (UP)
+                if (myPosition + 2 < trackLength) {
                     return "UP";
                 }
+            } else if (myPosition + 2 < trackLength && gpu.charAt(myPosition + 2) == '#') {
+                // If the cell after next is a hurdle, move two spaces to the right (RIGHT)
+                return "RIGHT";
+            } else if (myPosition + 3 < trackLength && gpu.charAt(myPosition + 3) == '#') {
+                // If the cell two spaces after next is a hurdle, move two spaces down (DOWN)
+                return "DOWN";
             }
-
-            // Default action: move one space to the right
-            return "RIGHT";
         }
 
-        private static List<Integer> getGamesToConsider (String[]gpus){
-            // Find the games with the least obstacles
-            List<Integer> gamesToConsider = new ArrayList<>();
-            int[] obstacleCounts = new int[gpus.length];
-
-            // Calculate obstacle counts for each game
-            for (int i = 0; i < gpus.length; i++) {
-                String gpu = gpus[i];
-                int obstacleCount = 0;
-
-                // Count obstacles in the game
-                for (int j = 0; j < gpu.length(); j++) {
-                    if (gpu.charAt(j) == '#') {
-                        obstacleCount++;
-                    }
-                }
-
-                obstacleCounts[i] = obstacleCount;
-            }
-
-            // Find indices of games with the least obstacles
-            int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, min3 = Integer.MAX_VALUE;
-            int idx1 = -1, idx2 = -1, idx3 = -1;
-
-            for (int i = 0; i < obstacleCounts.length; i++) {
-                if (obstacleCounts[i] < min1) {
-                    min3 = min2;
-                    idx3 = idx2;
-                    min2 = min1;
-                    idx2 = idx1;
-                    min1 = obstacleCounts[i];
-                    idx1 = i;
-                } else if (obstacleCounts[i] < min2) {
-                    min3 = min2;
-                    idx3 = idx2;
-                    min2 = obstacleCounts[i];
-                    idx2 = i;
-                } else if (obstacleCounts[i] < min3) {
-                    min3 = obstacleCounts[i];
-                    idx3 = i;
-                }
-            }
-
-            // Add the indices of the 3 games to consider
-            gamesToConsider.add(idx1);
-            gamesToConsider.add(idx2);
-            gamesToConsider.add(idx3);
-
-            return gamesToConsider;
-        }
-
-        String message;
-        Action action;
-        int[][] medals;
-
-    public Player() {
-        }
-
-        public void init ( int gameCount){
-            medals = new int[gameCount][3];
-        }
-
-        public int getPoints () {
-            int p = 1;
-            for (int i = 0; i < medals.length; ++i) {
-                p *= (3 * medals[i][0] + medals[i][1]);
-            }
-            return p;
-        }
-
-        public int getExpectedOutputLines () {
-            return 1;
-        }
-
-        public void reset () {
-            this.message = null;
-            this.action = null;
-        }
-
-        public void setMessage (String message){
-            this.message = message;
-
-        }
-
-        public void setAction (Action button){
-            this.action = button;
-        }
-
-        public Action getAction () {
-            return action;
-        }
-
-        public int[] getMedalsTotal () {
-            int[] total = new int[3];
-            for (int i = 0; i < medals.length; ++i) {
-                int golds = medals[i][0];
-                int silvers = medals[i][1];
-                int bronzes = medals[i][2];
-                total[0] += golds;
-                total[1] += silvers;
-                total[2] += bronzes;
-            }
-            return total;
-        }
-
-        public String getScoreText () {
-            List<String> minigameScores = new ArrayList<>(medals.length);
-            for (int i = 0; i < medals.length; ++i) {
-                int golds = medals[i][0];
-                int silvers = medals[i][1];
-                if (golds == 0 && silvers > 1) {
-                    minigameScores.add(String.format("%d🥈", silvers));
-                } else if (golds > 0 && silvers == 0) {
-                    minigameScores.add(String.format("%d🥇", golds));
-                } else if (golds == 0 && silvers == 0) {
-                    minigameScores.add("0");
-                } else {
-                    minigameScores.add(String.format("%d🥇+%d🥈", golds, silvers));
-                }
-            }
-            return minigameScores.stream().collect(Collectors.joining(" * ")) + " = " + getPoints();
-        }
+        // Default action: move one space to the left (LEFT)
+        return "LEFT";
     }
+}
 
-    enum Action {
-        UP, DOWN, LEFT, RIGHT
-    }
+enum Action {
+    UP, DOWN, LEFT, RIGHT
+}
