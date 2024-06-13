b8a954df7d3fa17f7e57ab707a910edf73d477e9
DacFlorent <Dac.florent@gmail.com>
2024-06-13 12:37:49 +0200

    reprise a zero du projet jeux 2 simplifié

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,11 @@
-import java.util.*;
-
 class Player {
 
-    static List<Integer> obstacles = new ArrayList<>();
+    static java.util.List<Integer> obstacles = new java.util.ArrayList<>();
+    static boolean isStunned = false;
 
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+        java.util.Scanner in = new java.util.Scanner(System.in);
+
         int playerIdx = in.nextInt();
         int nbGames = in.nextInt();
         if (in.hasNextLine()) {
@@ -14,6 +14,8 @@ class Player {
 
         // game loop
         while (true) {
+            isStunned = false; // Reset stun status for the new game
+
             // Read score information for each player
             String[] scoreInfo = new String[3];
             for (int i = 0; i < 3; i++) {
@@ -34,10 +36,10 @@ class Player {
             }
 
             // Pre-calculate obstacles
-            calculateObstacles(gpu[0]);
+            calculateObstacles(gpu[playerIdx]);
 
             // Choose the action for the current game state
-            String direction = chooseDirection(playerIdx, gpu[0], registers[playerIdx][0], registers[playerIdx][3]);
+            String direction = chooseDirection(playerIdx, gpu[playerIdx], registers[playerIdx][0], registers[playerIdx][3], 0);
 
             // Output the chosen direction
             System.out.println(direction);
@@ -54,14 +56,20 @@ class Player {
         }
     }
 
-    private static String chooseDirection(int playerIdx, String gpu, int myPosition, int myStunCount) {
+    private static String chooseDirection(int playerIdx, String gpu, int myPosition, int myStunCount, int gameIdx) {
         // If stunned, do nothing
-        if (myStunCount > 0) {
+        if (isStunned && gameIdx == playerIdx) {
             return "LEFT";
         }
 
         // Check the track length
         int trackLength = gpu.length();
+
+        // Check if the player is stunned in this game
+        if (gameIdx == playerIdx && myStunCount > 0) {
+            isStunned = true;
+            return "LEFT";
+        }
 
         // Check up to myPosition + 3 for potential obstacles
         for (int i = myPosition + 1; i <= myPosition + 3 && i < trackLength; i++) {
